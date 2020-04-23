d2f09c0ce658fd85f084d87004c872e527fb60b4
Emanuel Damian Paz <emanuel.paz@despegar.com>
2020-04-23 20:43:44 -0300

    Hice un html con el js

--- calculadora.js
+++ calculadora.js
@@ -5,3 +5,14 @@ function suma (x,y){
 function resta (x,y){
    return x-y;
 }
+
+function main(){
+	document.getElementById('suma_resultado1').innerHTML = suma(1,1);
+    document.getElementById('suma_resultado2').innerHTML = suma(2,2);
+	document.getElementById('suma_resultado3').innerHTML = suma(3,3);
+
+	document.getElementById('resta_resultado').innerHTML = resta(2,1);
+
+};
+
+main();
